ec0ca415b5252a0f22f3746cdc3d6a89961ebbb3
Dave Smith <lah4life@gmail.com>
2017-02-20 21:52:24 +0000

    Added multer and file size return logic

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,20 @@
 'use strict'
+const path = require('path');
+const multer = require('multer');
+const upload = multer({ dest: './uploads/'});
 
 module.exports = (app)=>{
+    
+    app.post('/upload', upload.single('file'), (req, res)=>{
+        let fileSize = {size: req.file.size};
+        res.status(200).send(fileSize);
+    });
+    
+    app.get('/upload', (req, res)=>{
+        let uploadPage = path.resolve(__dirname, '../../views/upload.html');
+        res.status(200).sendFile(uploadPage);
+    });
+    
     app.get('/', (req, res)=>{
         res.status(200).send('Hello');
     });
